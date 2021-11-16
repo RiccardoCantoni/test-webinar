95bf745d423cb95c736c0215e90f2467f0b41ec8
riccardo cantoni <rcantoni@deloitte.it>
2021-11-16 14:23:48 +0100

    env

---
+++ environments/hotfix/index.js
@@ -0,0 +1,16 @@
+const API = require('./api.js')
+
+async function newVisit(timestamp, lang) {
+  try {
+    const res = await API.post(
+      'https://www.fantozziefigli.com/services/new-visit',
+      { timestamp, lang }
+    )
+    console.log('res.success', res.success)
+    return res.success
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+module.exports = newVisit
